d78781133fa9c1feae8c38421f4331dc05d7052e
Milton <milton.cneto@gmail.com>
2022-02-08 20:53:48 -0300

    Product repository, service, resourve, GET ok

---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function ButtonIcon() {
+    return (
+      <>
+       <button className="btn btn-primary btn-icon">
+           <h6>INICIE AGORA SUA BUSCA</h6>
+       </button>
+      </>
+    );
+  }
+
+  export default ButtonIcon;
--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButtonIcon from 'components/ButtonIcon';
 import Navbar from 'components/Navbar';
+
+import './style.css';
 
 function Home() {
   return (
@@ -9,6 +12,8 @@ function Home() {
         <div className="home-card">
             <div className="home-content-container">
                 <h1>Conheça o melhor catálogo de produtos</h1>
+                <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
+                <ButtonIcon />
             </div>
             <div className="home-image-container">
                 <MainImage />
